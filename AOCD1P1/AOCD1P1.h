ec1fae00000e8f42462dbaff1eefd07baa120dca
Venkatesh Maharajan <venkatesh.xaviers@gmail.com>
2025-02-23 02:19:47 -0800

    day1 completed

---
+++ AOCD1P1/AOCD1P1.h
@@ -0,0 +1,12 @@
+﻿// AOCD1P1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+// TODO: Reference additional headers your program requires here.
---
+++ AOCD1P2/AOCD1P2.h
@@ -0,0 +1,13 @@
+﻿// AOCD1P2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <map>
+
+// TODO: Reference additional headers your program requires here.
